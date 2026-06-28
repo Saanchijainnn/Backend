42c02616ed070f22b73baf70decaf79f2626486f
saanchijainnn <saanchi2315@gmail.com>
2026-06-29 00:50:32 +0530

    node js basics

---
+++ script2.js
@@ -0,0 +1,27 @@
+//Node.js basics
+//introduction to node.js
+//installing node.js and npm.
+//working with modules
+
+//file system soperations 
+//understanding HHTP module
+
+
+/*node js is not a programming language not a technology not a framework not a software 
+this is a js runtime environment 
+
+js se backend nhi ban sakta kyuki js ke pass vo functionalities hi nhhi h jinse backend banta h 
+
+ryan dahl ne socha js se backend banna chahiye 
+google chrome ka v8 engine ka code open source h and ryan dahl us code men chedkhaani kar raha h 
+fir usne socha hume to js m code karna h kyuki chrome ka v8 is in c++ 
+
+
+hum js ka code likhenge jo i wrapper layer of js receive karegi vo code v8 ke cpp ingine k saath ek server create karega
+
+
+
+v8 wrapped in js is called node.js 
+
+
+nodjs is a js runtime environment */

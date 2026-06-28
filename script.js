d492021536cdefbc4dcce0e89f71b1e40d254daa
saanchijainnn <saanchi2315@gmail.com>
2026-06-28 23:39:18 +0530

    about objects

--- script.js
+++ script.js
@@ -4,37 +4,48 @@
 //async js coding
 //for each,map ,filter,find,indexOf
 
-var arr = [1,2,3,4]
+/*var arr = [1,2,3,4]
 
-//forEach
-/*arr.forEach(function(val){
+forEach
+arr.forEach(function(val){
    console.log (val+" "+ "hello");
-})*/
+})
 
-//map: map aapko ek aur array dega usme unte hi members honge jitne isme h. foeeach ki tarah chalta h par vo ek naya array deta h forech nhi deta 
+map: map aapko ek aur array dega usme unte hi members honge jitne isme h. foeeach ki tarah chalta h par vo ek naya array deta h forech nhi deta 
 
-/*var ans=arr.map(function(val){
+var ans=arr.map(function(val){
     return val *3;
 })
 
-console.log(ans)*/
+console.log(ans)
 
 
-//filter:  hume isse aisa array milta h jo existung arrays e chota ho sakta h ya use barabar ho sakta h 
+filter:  hume isse aisa array milta h jo existung arrays e chota ho sakta h ya use barabar ho sakta h 
 
-/*var ans = arr.filter(function(val){
+var ans = arr.filter(function(val){
     if(val > 3){return true;}
     else return false;
 })
-console.log(ans)*/
+console.log(ans)
 
 
-//find: iisse dhoodhte h, aur jo sabse pehle hoga vhi return ho jaayega 
+find: iisse dhoodhte h, aur jo sabse pehle hoga vhi return ho jaayega 
 
-/*var ans= arr.find(function(val){
+var ans= arr.find(function(val){
     if(val === 2)return val;
 })
-console.log(ans)*/
+console.log(ans)
 
-//indexOf: jo banda hoga uska index dega nahi to -1 print hoga 
-/*console.log(arr.indexOf(4))*/
+indexOf: jo banda hoga uska index dega nahi to -1 print hoga 
+console.log(arr.indexOf(4))*/
+
+
+/*{}: blank object, ye ek key value pair hota h, left side m key hogi aur right side m value hogi hamesha { a: 12}
+
+two ways to search
+var obj={
+    name: "saanchi"
+}
+obj.name, obj[name]*
+
+Object.freeze(obj): ab hium value change nhi kar sakte */
